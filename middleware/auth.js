a9e850565336199f9150f8f648bd1dcc74572460
Nelcy17 <150636101+Nelcy17@users.noreply.github.com>
2025-01-30 20:47:52 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+    const token = req.header("x-auth-token");
+
+    if (!token) return res.status(401).json({
+        msg: "No token authorization denied!"
+    });
+
+    try {
+        const decoded = jwt.verify(token, "Nelcy07");
+        req.user = decoded;
+        next();
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({
+            msg: "Token is not valid!"
+        })
+    }
+
+}
+
+module.exports = auth;
